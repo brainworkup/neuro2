e361103f619798ddba780991118fc54e126cecab
brainworkup <j.trampush@gmail.com>
2025-06-29 08:11:13 -0700

    fixing build warnings

---
+++ R/imports.R
@@ -0,0 +1,12 @@
+#' @importFrom fs dir_create dir_exists path
+#' @importFrom janitor clean_names
+#' @importFrom kableExtra kable_styling column_spec
+#' @importFrom knitr kable
+#' @importFrom readxl read_excel
+#' @importFrom stringr str_to_title str_trim str_replace
+#' @importFrom tidyselect all_of contains starts_with
+#' @importFrom tools file_path_sans_ext
+#' @importFrom usethis use_package use_data
+#' @importFrom utils installed.packages packageVersion
+#' @importFrom xfun session_info file_ext
+NULL
