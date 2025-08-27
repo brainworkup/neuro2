c0b844442c23ae45319da70c4f51413341f737c5
brainworkup <j.trampush@gmail.com>
2025-08-27 03:48:26 -0700

    removed .dbxignore

--- run.R
+++ run.R
@@ -1,12 +1,17 @@
 # Load the neuro2 package
 library(neuro2)
 
+# Source the required modules
+source("R/workflow_utils.R")
+source("R/workflow_config.R")
+source("R/WorkflowRunnerR6.R")
+
 # Load configuration (this will use default config.yml if no args provided)
 config <- load_workflow_config("config.yml")
 
 # Create and run the workflow
 workflow <- WorkflowRunnerR6$new(config)
-result <- workflow$run_workflow()
+result <- workflow$run()
 
 # Print summary
 workflow$print_summary(result)
